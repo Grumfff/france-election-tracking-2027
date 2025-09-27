2d47cd836a4ede2be9d462be7ed39713d9f51cec
Boris Vilboux <boris.vilboux@edf-re.fr>
2025-09-27 19:30:05 +0200

    premier graphe

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,7 +3,42 @@ import type { EChartsOption } from 'echarts'
 import echartsConfig from '../assets/data/echarts_config.json'
 
 export function Home() {
-  const chartOption: EChartsOption = echartsConfig as EChartsOption
+  const chartOption: EChartsOption = {
+    ...echartsConfig,
+    grid: {
+      ...echartsConfig.grid,
+      top: '10%',
+      bottom: '8%'
+    },
+    legend: {
+      ...echartsConfig.legend,
+      bottom: '2%'
+    },
+    series: [
+      ...echartsConfig.series.map((series) => {
+        const { itemStyle, ...seriesWithoutItemStyle } = series
+        return seriesWithoutItemStyle
+      }),
+      {
+        type: 'line',
+        markLine: {
+          data: [{
+            xAxis: 50,
+            lineStyle: {
+              color: '#000',
+              width: 2,
+              type: 'solid'
+            },
+            label: {
+              show: false
+            }
+          }],
+          symbol: 'none'
+        },
+        data: []
+      }
+    ]
+  } as unknown as EChartsOption
 
   return (
     <Chart option={chartOption} />
